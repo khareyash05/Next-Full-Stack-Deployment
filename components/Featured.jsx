b430a96ae726c1636a5f13d06365e1209a5384ac
khareyash05 <khareyash05@gmail.com>
2022-11-08 23:26:32 +0530

    featured section

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,16 +1,38 @@
 import styles from '../styles/Featured.module.css';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const Featured = () => {
+    const [index,setIndex] = useState(0)
+    const images = [
+        '/images/featured.png',
+        '/images/featured2.png',
+        '/images/featured3.png'
+    ];
+    const handleArrow = (direction)=>{
+        if(direction ==='l'){
+            setIndex(index !== 0 ? index-1 : 2)
+        }
+        if(direction ==='r'){
+            setIndex(index !== 2 ? index+1 : 0)
+        }
+    }
+    console.log(index);
     return ( 
         <div className={styles.container}>
-            <Image src="/images/arrowl.png" layout='fill'/>
-            <div className={styles.wrapper}>
-                <div className={styles.imgContainer}>
-                <Image src="/images/arrowl.png" layout='fill'/>
-                </div>
+            <div className={styles.arrowContainer}>
+                <Image src="/images/arrowl.png" layout='fill' style={{left:0}} onClick={()=> handleArrow("l")}/>
             </div>
-            <Image src="/images/arrowr.png" layout='fill'/>
+            <div className={styles.wrapper}>
+                {images.map((image,i)=>                    
+                    <div className={styles.imgContainer} key={i}>
+                        <Image src={image} layout='fill' objectFit='contain'/>
+                    </div>
+                )}
+            </div>
+            <div className={styles.arrowContainer}>
+                <Image src="/images/arrowr.png" layout='fill' style={{right:0}} onClick={()=> handleArrow("r")}/>                
+            </div>
         </div>
     );
 }
